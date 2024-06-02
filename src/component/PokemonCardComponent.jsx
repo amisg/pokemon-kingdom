c916c0a68291c2438fba588159e3a6455531608e
amisg <aamithsg18@gmail.com>
2024-06-02 12:43:04 +0530

    pokemon card handling  and rendering done

--- src/component/PokemonCardComponent.jsx
+++ src/component/PokemonCardComponent.jsx
@@ -1,15 +1,48 @@
-export default function PokemonCardComponent(props) {
-	const { id, image, name, type } = props;
+import { useState } from "react";
+import styles from "./PokemonCardComponent.module.css";
+import PokemonModalComponent from "./PokemonModalComponent";
+import BackDrop from "./BackDrop";
+
+function PokemonCardComponent(props) {
+	const { pokemon } = props;
+
+	const { id, name, type, image } = pokemon;
+
+	const [modal, setModal] = useState(false);
+
+	// CARD FOR THE POKEMON.
 	return (
 		<>
-			<div className="card-container">
-				<div className="number">{id}</div>
-				<img src={image} alt="pokemonImage" />
+			<div className={` ${styles["card-container"]} ${styles[type]} `}>
+				<div className={styles.number}>#{id}</div>
+				<img className={styles.image} src={image} alt="Pokemon" />
 				<div className="detail-wrapper">
 					<h3>{name}</h3>
 					<p>Type: {type}</p>
 				</div>
+
+				<div
+					onClick={() => {
+						setModal((old) => !old);
+					}}
+					className={styles.knowmore}>
+					Know More...
+				</div>
 			</div>
+
+			{/* Modal  */}
+			{/* {modal && <PokemonModalComponent pokemon={pokemon} />} */}
+			{modal && (
+				<BackDrop onClose={() => setModal(false)}>
+					{" "}
+					<PokemonModalComponent
+						onClose={() => setModal(false)}
+						pokemon={pokemon}
+					/>{" "}
+				</BackDrop>
+			)}
 		</>
 	);
 }
+
+export default PokemonCardComponent;
--- src/pages/PokemonKingdomMainPage.jsx
+++ src/pages/PokemonKingdomMainPage.jsx
@@ -1,42 +1,86 @@
 import { useEffect, useState } from "react";
+import styles from "./PokemonKingdomMainPage.module.css";
+import PokemonCardComponent from "../component/PokemonCardComponent";
 
-export default function PokemonKingdomMainPage() {
+function PokemonKingdomMainPage(props) {
+	const url =
+		"https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1";
+	const [nextUrl, setNextUrl] = useState(url);
+
+	console.log(nextUrl);
+
 	const [loading, setLoading] = useState(false);
+	let [pokemonData, setPokemonData] = useState([]);
+
+	async function fetchPokemonList() {
+		setLoading(true);
+
+		const response = await fetch(nextUrl);
+		let data = await response.json(); // [ {} ]
+		data = data[0]; // {}
+
+		console.log(data, "data");
+		setNextUrl(data.next);
+
+		const { results = [], next, count } = data;
+
+		const listOfAllPokemons = [];
+
+		for (let i = 0; i < results.length; i++) {
+			const pokemonSmallObj = results[i];
+			const { url: detailUrl, name } = pokemonSmallObj;
+
+			const response = await fetch(detailUrl);
+			let pokemonDetailData = await response.json();
+			pokemonDetailData = pokemonDetailData[0];
+
+			listOfAllPokemons.push(pokemonDetailData);
+		}
+		setPokemonData((oldData) => [...oldData, ...listOfAllPokemons]);
+
+		setLoading(false);
+	}
 
 	useEffect(() => {
-		const url =
-			"https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1";
-		async function fetchPokemon(params) {
-			setLoading(true);
-			const response = await fetch(url);
-			let data = await response.json();
-
-			data = data[0];
-			// console.log(data, "data");
-
-			const { results = [], name, count } = data;
-
-			for (let i = 0; i < results.length; i++) {
-				const pokemonSmallObj = results[i];
-				const { url: detailUrl, name } = pokemonSmallObj;
-				const response = await fetch(detailUrl);
-				const pokemonDetailData = await response.json();
-				console.log(pokemonDetailData, ":::NAME:::", name);
-				setLoading(false);
-			}
-		}
-		fetchPokemon();
+		fetchPokemonList();
 	}, []);
 
 	return (
 		<>
 			{loading ? (
-				<h1>Loading....</h1>
+				<h1>LOADING........</h1>
 			) : (
-				<div>
-					<h1>Pokemon Kingdom</h1>
-				</div>
+				<>
+					<h1>Pokemon Kingdom Welcome</h1>
+
+					{/* POKEMON */}
+
+					<div className={styles["app-container"]}>
+						<div className={styles["pokemon-container"]}>
+							{pokemonData.map((pokemon) => {
+								const { id } = pokemon;
+
+								return (
+									<div key={id}>
+										<PokemonCardComponent pokemon={pokemon} />
+									</div>
+								);
+							})}
+						</div>
+
+						<button
+							onClick={() => {
+								console.log("Need to load More pokemons");
+								fetchPokemonList();
+							}}
+							className={styles.loadmore}>
+							Load More
+						</button>
+					</div>
+				</>
 			)}
 		</>
 	);
 }
+
+export default PokemonKingdomMainPage;
